c640c97da604a086614cba6eb2aa5a9156605ff9
lymsun <lymsunsheep@sina.cn>
2016-11-11 14:27:51 +0800

    angularJs Demo1 20161111

---
+++ app/js/angularJsDemo1.js
@@ -0,0 +1,40 @@
+/**
+ *
+ * File: angularJsDemo1.js
+ * Author: Lym
+ * Created Date: 2016/10/28.
+ * Function:
+ *
+ */
+
+var app = angular.module('angularJsDemo1', []).controller('helloController', function($scope, $timeout){
+   $scope.person = {
+       name : ""
+   };
+    var updateClock = function(){
+        var tempDate = new Date();
+        var tempMinutes = tempDate.getMinutes().toString()<10?"0"+tempDate.getMinutes().toString():tempDate.getMinutes().toString();
+        var tempSeconds = tempDate.getSeconds().toString()<10?"0"+tempDate.getSeconds().toString():tempDate.getSeconds().toString();
+        $scope.clock = {
+            now : tempDate.getHours().toString() + ":" + tempMinutes + ":" + tempSeconds
+        };
+        $timeout(function(){
+            updateClock();
+        }, 1000);
+        /*setInterval(function(){
+            $scope.$apply(updateClock);
+        }, 1000);*/
+    };
+
+    $scope.addMachine = {
+        count : 0,
+        addCount : function(amount){
+            this.count += amount;
+        },
+        subtractCount : function(amount){
+            this.count -= amount;
+        }
+    };
+
+    updateClock();
+});
