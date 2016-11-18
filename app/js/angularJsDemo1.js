a2c0cfcd973d182243d9684095f1cb77475190fd
lymsun <lymsunsheep@sina.cn>
2016-11-18 16:36:29 +0800

    angularJs Demo1 20161115

--- app/js/angularJsDemo1.js
+++ app/js/angularJsDemo1.js
@@ -52,3 +52,21 @@ var app = angular.module('angularJsDemo1', []).controller('helloController', fun
 
     updateClock();
 });
+
+app.directive('multipleEmail', [function () {
+    return {
+        require: "ngModel",
+        link: function (scope, element, attr, ngModel) {
+            if (ngModel) {
+                var emailsRegexp = /^([a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+)*[;；]?)+$/i;
+            }
+            var customValidator = function (value) {
+                var validity = ngModel.$isEmpty(value) || emailsRegexp.test(value);
+                ngModel.$setValidity("multipleEmail", validity);
+                return validity ? value : undefined;
+            };
+            ngModel.$formatters.push(customValidator);
+            ngModel.$parsers.push(customValidator);
+        }
+    };
+}]);
